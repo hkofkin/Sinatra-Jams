bffd6d02ce6f0590d067c5e119134c8cdf4fb1cd
hkofkin <hkofkin@gmail.com>
2020-06-04 17:38:38 -0400

    Update and Delete completed

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -19,4 +19,21 @@ class ArtistsController < ApplicationController
         redirect "/artists/#{@artist.id}"
     end
 
+    get '/artists/:id/edit' do 
+        @artist = Artist.find(params[:id])
+        erb :"artists/edit"
+    end
+
+    put '/artists/:id' do
+        @artist = Artist.find(params[:id])
+        @artist.update(params[:artist])
+        redirect "/artists/#{@artist.id}"
+    end
+
+    delete '/artists/:id' do
+        @artist = Artist.find(params[:id])
+        @artist.destroy
+        redirect "/artists"
+    end
+
 end
